446945b0232a23a179630dc77ecc32ca8c799bce
Ben Shyong <hello@benshyong.com>
2020-06-17 15:44:42 -0700

    handle openid users in User::Profile.new

--- app/services/users/profile.rb
+++ app/services/users/profile.rb
@@ -35,7 +35,7 @@ module Users
     private
 
     def validate!(user)
-      raise Common::Exceptions::ParameterMissing.new('user'), 'user' unless user&.class == User
+      raise Common::Exceptions::ParameterMissing.new('user'), 'user' unless user&.class == User || user&.class == OpenidUser
 
       user
     end
